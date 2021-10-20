6e7e2d52ccb354f960e94206fc1b7db5988452c3
ManuelGJesusOlmedo1207 <92826460+ManuelGJesusOlmedo1207@users.noreply.github.com>
2021-10-20 11:45:25 -0400

    Esta es la tarea de programación 1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Valor de las Variables
+
+        //a     
+        int Num2 = 40;
+        //b
+        int Num4 = 10;
+        //c
+        int Num1 = 35;
+
+        //Utilice los operadores aritméticos para poder mostrar un número que este conformado por tres dígitos:
+
+        //aacb
+        //Suma.
+        Console.WriteLine(Num2+Num2+Num1+Num4);
+
+        //bca
+        //Multiplicación y suma. 
+        Console.WriteLine(Num4*Num1+Num2);
+
+            
+        //bcbc
+        //Multiplicación, suma y división.
+        Console.WriteLine(Num4*Num1+Num4/Num1);
+
+        }
+    }
+}
